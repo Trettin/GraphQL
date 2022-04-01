1f976d87b2e2561e067a8e6ea910820cf845a7a2
Gabriel Trettin <gtcastro7@gmail.com>
2022-04-01 20:22:10 -0300

    created a query for highlightedProduct returning Product with a resolver to return the price with discount

--- schema-query/project/index.js
+++ schema-query/project/index.js
@@ -14,14 +14,33 @@ const typeDefs = gql`
     vip: Boolean
   }
 
+  type Product {
+    name: String!
+    price: Float!
+    discount: Int
+    priceWithDiscount: Float
+  }
+
   type Query {
     hello: String!
     currentHour: Date!
     loggedUser: User!
+    highlightedProduct: Product!
   }
 `;
 
 const resolvers = {
+  User: {
+    salary(user) {
+      return user.salary_in_real;
+    },
+  },
+  Product: {
+    priceWithDiscount(product) {
+      return (product.price * ((100 - product.discount) / 100)).toFixed(2);
+    },
+  },
+  // Query is the first entry and the methods have no params
   Query: {
     hello() {
       return "Hello Worl!";
@@ -35,8 +54,15 @@ const resolvers = {
         name: "Ana",
         email: "ana@gmail.com",
         age: 28,
-        salary: 2000.5,
+        salary_in_real: 2000.5,
         vip: true,
+      };
+    },
+    highlightedProduct() {
+      return {
+        name: "Ball",
+        price: 19.99,
+        discount: 10,
       };
     },
   },
@@ -48,5 +74,5 @@ const server = new ApolloServer({
 });
 
 server.listen().then(({ url }) => {
-  console.log(`Executando em ${url}`);
+  console.log(`Executing in ${url}`);
 });
