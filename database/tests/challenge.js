f4fd2469431a581c75642c79895aa06f8eec6a11
Gabriel Trettin <gtcastro7@gmail.com>
2022-09-18 20:45:30 -0300

    finished challenge code

--- database/tests/challenge.js
+++ database/tests/challenge.js
@@ -1,14 +1,44 @@
 const db = require("../config/db");
 
-async function saveUser(name, email, password) {}
+async function saveUser(name, email, password) {
+  const userExists = await db("users").where({ email }).first();
 
-async function saveProfile(name, label) {}
+  const userData = { name, password, email };
 
-async function addProfiles(user, ...profiles) {}
+  if (userExists) {
+    await db("users").where({ email }).update({ name: name });
+  } else {
+    await db("users").insert(userData);
+  }
+
+  const user = await db("users").where({ email }).first();
+  return user;
+}
+
+async function saveProfile(name, label) {
+  const profileExists = await db("profiles").where({ name }).first();
+
+  if (profileExists) {
+    await db("profiles").where({ name }).update({ label });
+  } else {
+    await db("profiles").insert({ name, label });
+  }
+
+  const profile = await db("profiles").where({ name }).first();
+  return profile;
+}
+
+async function addProfiles(user, ...profiles) {
+  const { id: user_id } = user;
+
+  for (profile of profiles) {
+    await db("users_profiles").insert({ user_id, profile_id: profile.id });
+  }
+}
 
 async function execute() {
-  const user = await saveUser("Ana", "ana@company.com", "123456");
-  const profileA = await saveProfile("hr", "People");
+  const user = await saveUser("Catarina", "catarina@company.com", "123456");
+  const profileA = await saveProfile("hr", "Prople");
   const profileB = await saveProfile("fin", "Financial");
 
   console.log(user);
