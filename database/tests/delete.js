843a1bd714b1a252ccaf701fcf227fcbaed755f6
Gabriel Trettin <gtcastro7@gmail.com>
2022-09-18 19:06:35 -0300

    created delete function for db

---
+++ database/tests/delete.js
@@ -0,0 +1,13 @@
+const db = require("../config/db");
+
+// Delete user by id
+// db("users")
+//   .where({ id: 1 })
+//   .delete()
+//   .then((res) => console.log(res));
+
+// Delete all
+// db("profiles")
+//   .delete()
+//   .then((res) => console.log(res))
+//   .finally(() => db.destroy());
