b9cac9f851c136d80976f4a2558d0af379ec3ef4
Gabriel Trettin <gtcastro7@gmail.com>
2022-09-19 23:45:41 -0300

    implemented user mutation newUser with 2 codes, one commented code with if else and the other which isn't commented using knex builder with the if else inside of it, much cleaner code

--- database/db-challenge/resolvers/Mutation/user.js
+++ database/db-challenge/resolvers/Mutation/user.js
@@ -1,8 +1,63 @@
 const db = require("../../config/db");
+const { profiles: getUserProfiles } = require("../Type/User");
 
 module.exports = {
-  async newUser(_, { data }) {
-    // implement
+  async newUser(_, { data: { name, email, password, profiles } }) {
+    if (!name || !email || !password || !profiles) {
+      throw new Error(
+        "Fields: 'name', 'email', 'password' and 'profiles' are mandatory."
+      );
+    }
+    const userExists = await db("users").where({ email }).first();
+
+    if (userExists) {
+      throw new Error("Duplicated email.");
+    }
+
+    await db("users").insert({ name, email, password });
+
+    const user = await db("users").where({ email }).first();
+
+    if (profiles.length) {
+      for (let userProfile of profiles) {
+        await db("profiles")
+          .where((builder) => {
+            if (userProfile.name) {
+              builder.where({ name: userProfile.name });
+            } else {
+              builder.where({ id: userProfile.id });
+            }
+          })
+          .first()
+          .then((profile) =>
+            db("users_profiles").insert({
+              user_id: user.id,
+              profile_id: profile.id,
+            })
+          );
+
+        // if (userProfile.id) {
+        //   await db("users_profiles").insert({
+        //     user_id: user.id,
+        //     profile_id: userProfile.id,
+        //   });
+        // } else {
+        //   const profile = await db("profiles")
+        //     .where({
+        //       name: userProfile.name,
+        //     })
+        //     .first();
+
+        //   await db("users_profiles").insert({
+        //     user_id: user.id,
+        //     profile_id: profile.id,
+        //   });
+        // }
+      }
+    }
+    const userProfiles = await getUserProfiles(user);
+
+    return { ...user, profiles: userProfiles };
   },
   async deleteUser(_, { filter }) {
     // implement
--- database/db-challenge/resolvers/Type/User.js
+++ database/db-challenge/resolvers/Type/User.js
@@ -2,9 +2,6 @@ const db = require("../../config/db");
 
 module.exports = {
   async profiles(user) {
-    // const userProfiles = await db.select('profile_id').from("users_profiles").where({ user_id: user.id });
-    // console.log("teste", userProfiles);
-
     return await db("profiles").whereIn(
       "id",
       db.select("profile_id").from("users_profiles").where({ user_id: user.id })
