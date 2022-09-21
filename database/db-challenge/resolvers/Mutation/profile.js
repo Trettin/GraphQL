2e6b961f0bbd2ded28d37bdded0bfc8f73f1ccb0
Gabriel Trettin <gtcastro7@gmail.com>
2022-09-21 19:35:49 -0300

    implemented newProfile resolver

--- database/db-challenge/resolvers/Mutation/profile.js
+++ database/db-challenge/resolvers/Mutation/profile.js
@@ -1,9 +1,27 @@
 const db = require("../../config/db");
+const { profile } = require("../Query/profile");
 
 module.exports = {
   async newProfile(_, { data }) {
-    // implementar
+    try {
+      if (!data || !data.name || !data.label)
+        throw new Error("Please send a Name and Label for the Profile.");
+
+      const { name } = data;
+
+      const profileExist = await profile(_, { filter: { name } });
+      if (profileExist) {
+        throw new Error("Duplicated Profile name. Please choose another name.");
+      }
+
+      await db("profiles").insert({ ...data });
+
+      return await profile(_, { filter: { name } });
+    } catch (error) {
+      throw new Error(error);
+    }
   },
+
   async deleteProfile(_, { filter }) {
     // implementar
   },
